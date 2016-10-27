65233bd393ef278f633ad87e4e254706006f6430
Sunday Ayandokun <sunday.ayandokun@zerotech.ee>
2016-10-28 01:11:58 +0300

    Added satellizer server implementation files

--- src/main/java/com/sunnepah/savewithme/SavewithmeApplication.java
+++ src/main/java/com/sunnepah/savewithme/SavewithmeApplication.java
@@ -1,29 +1,77 @@
 package com.sunnepah.savewithme;
 
+import com.sunnepah.savewithme.db.UserDAO;
+import com.sunnepah.savewithme.resources.ClientResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.client.JerseyClientBuilder;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class SavewithmeApplication extends Application<SavewithmeConfiguration> {
+import javax.ws.rs.client.Client;
 
-    public static void main(final String[] args) throws Exception {
-        new SavewithmeApplication().run(args);
-    }
+import com.sunnepah.savewithme.auth.AuthFilter;
+import com.sunnepah.savewithme.core.User;
+import com.sunnepah.savewithme.resources.AuthResource;
+import com.sunnepah.savewithme.resources.UserResource;
 
-    @Override
-    public String getName() {
-        return "Savewithme";
-    }
+public class SaveWithMeApplication extends Application<SaveWithMeConfiguration> {
+  public static void main(final String[] args) throws Exception {
+    new SaveWithMeApplication().run(args);
+  }
 
-    @Override
-    public void initialize(final Bootstrap<SavewithmeConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+  private final HibernateBundle<SaveWithMeConfiguration> hibernateBundle =
+      new HibernateBundle<SaveWithMeConfiguration>(User.class) {
+        @Override
+        public DataSourceFactory getDataSourceFactory(final SaveWithMeConfiguration configuration) {
+          return configuration.getDataSourceFactory();
+        }
+      };
 
-    @Override
-    public void run(final SavewithmeConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+  @Override
+  public String getName() {
+    return "SaveWithMe";
+  }
 
+  @Override
+  public void initialize(final Bootstrap<SaveWithMeConfiguration> bootstrap) {
+    bootstrap.addBundle(new MigrationsBundle<SaveWithMeConfiguration>() {
+      @Override
+      public DataSourceFactory getDataSourceFactory(final SaveWithMeConfiguration configuration) {
+        return configuration.getDataSourceFactory();
+      }
+    });
+
+    bootstrap.addBundle(hibernateBundle);
+
+    bootstrap.addBundle(new AssetsBundle("/assets/app.js", "/app.js", null, "app"));
+    bootstrap.addBundle(new AssetsBundle("/assets/stylesheets", "/stylesheets", null, "css"));
+    bootstrap.addBundle(new AssetsBundle("/assets/directives", "/directives", null, "directives"));
+    bootstrap
+        .addBundle(new AssetsBundle("/assets/controllers", "/controllers", null, "controllers"));
+    bootstrap.addBundle(new AssetsBundle("/assets/services", "/services", null, "services"));
+    bootstrap.addBundle(new AssetsBundle("/assets/vendor", "/vendor", null, "vendor"));
+    bootstrap.addBundle(new AssetsBundle("/assets/partials", "/partials", null, "partials"));
+  }
+
+  @Override
+  public void run(final SaveWithMeConfiguration configuration, final Environment environment)
+      throws ClassNotFoundException {
+
+    final UserDAO dao = new UserDAO(hibernateBundle.getSessionFactory());
+    final Client client =
+        new JerseyClientBuilder(environment).using(configuration.getJerseyClient())
+            .build(getName());
+    final SaveWithMeConfiguration.ClientSecretsConfiguration clientSecrets = configuration.getClientSecrets();
+
+    environment.jersey().register(new ClientResource());
+    environment.jersey().register(new UserResource(dao));
+    environment.jersey().register(new AuthResource(client, dao, clientSecrets));
+
+    environment.servlets().addFilter("AuthFilter", new AuthFilter())
+        .addMappingForUrlPatterns(null, true, "/api/me");
+  }
 }
--- src/main/java/com/sunnepah/savewithme/SavewithmeConfiguration.java
+++ src/main/java/com/sunnepah/savewithme/SavewithmeConfiguration.java
@@ -1,10 +1,93 @@
 package com.sunnepah.savewithme;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import io.dropwizard.client.JerseyClientConfiguration;
+import io.dropwizard.db.DataSourceFactory;
 
-public class SavewithmeConfiguration extends Configuration {
-    // TODO: implement service configuration
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SaveWithMeConfiguration extends Configuration {
+	
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private JerseyClientConfiguration httpClient = new JerseyClientConfiguration();
+    
+    @Valid
+    @NotNull
+    @JsonProperty
+    private ClientSecretsConfiguration clientSecrets = new ClientSecretsConfiguration();
+    
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
+
+	public JerseyClientConfiguration getJerseyClient() {
+        return httpClient;
+    }
+	
+	public ClientSecretsConfiguration getClientSecrets() {
+		return clientSecrets;
+	}
+	
+	public static class ClientSecretsConfiguration {
+		
+		@NotBlank
+		@JsonProperty
+		String facebook;
+		
+		@NotBlank
+		@JsonProperty
+		String google;
+		
+		@NotBlank
+		@JsonProperty
+		String linkedin;
+		
+		@NotBlank
+		@JsonProperty
+		String github;
+		
+		@NotBlank
+		@JsonProperty
+		String foursquare;
+		
+		@NotBlank
+		@JsonProperty
+		String twitter;
+		
+		public String getFacebook() {
+			return facebook;
+		}
+		
+		public String getGoogle() {
+			return google;
+		}
+		
+		public String getLinkedin() {
+			return linkedin;
+		}
+		
+		public String getFoursquare() {
+			return foursquare;
+		}
+		
+		public String getTwitter() {
+			return twitter;
+		}
+	}
 }
