6758f385b2650af1c048c71077098ffaf66bef83
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-11-04 00:05:45 +0200

    Added .jar to snapshot filename

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar target/Savewithme-1.0-SNAPSHOT db migrate config.yml && java $JAVA_OPTS -Ddw.server.connector.port=$PORT -jar target/Savewithme-1.0-SNAPSHOT server config.yml
+web: java $JAVA_OPTS -jar target/Savewithme-1.0-SNAPSHOT.jar db migrate config.yml && java $JAVA_OPTS -Ddw.server.connector.port=$PORT -jar target/Savewithme-1.0-SNAPSHOT.jar server config.yml
--- src/main/java/com/sunnepah/savewithme/db/MongoDAO.java
+++ src/main/java/com/sunnepah/savewithme/db/MongoDAO.java
@@ -1,8 +1,35 @@
 package com.sunnepah.savewithme.db;
 
+import com.mongodb.DB;
+import com.mongodb.MongoURI;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.Set;
+
 /**
  * Created by sunnepah on 03/11/2016.
  * Sunday Ayandokun @sundayayandokun
  */
 public class MongoDAO {
+
+    public void sampleMongo() throws URISyntaxException {
+
+        MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
+
+        DB db = null;
+
+        try {
+
+            db = mongoURI.connectDB();
+            db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Unknown host");
+        }
+
+        //Use the db object to talk to MongoDB
+        Set<String> colls = db.getCollectionNames();
+    }
 }
